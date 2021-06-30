ce7247a84c0197d9123171c1739122ac75d41880
송건우 <skw0616@naver.com>
2021-06-30 17:43:39 +0900

    New

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    // vue
+    //'plugin:vue/vue3-essential', // 1
+    'plugin:vue/vue3-strongly-recommended', //2
+    //'plugin:vue/vue3-recommended',  //3
+    // js 
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
+    
+  }
+}
